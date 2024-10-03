af30910765421cb6ddbb3872c8cc7e2c002e8ed1
ahmed <bejaouiam25@gmail.com>
2024-10-03 19:04:32 +0100

    second

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-const conn = mysql.createConnection({
+const db = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
@@ -14,11 +14,12 @@ const conn = mysql.createConnection({
   database: "Restaurants",
 });
 
-conn.connect((err) => {
-  if (err) throw err;
+db.connect((err) => {
+  if (err) {
+    console.log("error: " + err);
+  }
   console.log("MySql has connected successfully :) ");
 });
-//the setting the place of the 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     return cb(null, "./public/images");
@@ -28,43 +29,81 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
+//Add a Destination
 app.post(
   "/addDestination",
   upload.fields([{ name: "image" }, { name: "menu" }]),
   (req, res) => {
     const { name, tables, adresse, description } = req.body;
-    const image = req.files['image'] ? `/images/${req.files['image'][0].filename}` : null; // Handle destination image
-  const menu = req.files['menu'] ? `/images/${req.files['menu'][0].filename}` : null; // Handle menu image
-
-    const sql ="INSERT INTO Destination (name, tables, image, adresse, description,menu) VALUES (?,?,?,?,?,?)";
-    conn.query(
+    const image = req.files["image"]
+      ? `/images/${req.files["image"][0].filename}`
+      : null;
+    const menu = req.files["menu"]
+      ? `/images/${req.files["menu"][0].filename}`
+      : null;
+    const sql =
+      "INSERT INTO Destination (name, tables, image, adresse, description,menu) VALUES (?,?,?,?,?,?)";
+    db.query(
       sql,
-      [name, tables, image, adresse, description,menu],
+      [name, tables, image, adresse, description, menu],
       (err, result) => {
         if (err) {
-          return res.json({ err });
+          return res.json({ err: err });
         }
         res.json({ message: "Destination added successfully", result });
       }
     );
   }
 );
-
+//Delete a destination
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "Delete from Destination where id = ?";
+  db.query(sql, id, (err, result) => {
+    if (err) {
+      res.json({ err: err });
+    }
+    res.status(200).json({ message: "Deleted successfully" });
+  });
+});
+//get all destinations
 app.get("/destinations", (req, res) => {
   const sql = "Select * from Destination";
-  conn.query(sql, (err, result) => {
+  db.query(sql, (err, result) => {
     if (err) {
       return res.json({ err: err });
     }
     res.json(result);
   });
 });
-
+//add a reservation
+app.post("/addReservation/:idDestination", (req, res) => {
+  const idDestination = req.params.idDestination;
+  const date = new Date().toLocaleString();
+  const { idClient, numberOfPersons } = req.body;
+  const sql =
+    "Insert into Reservation (idClient, idRestaurant, numberOfPersons, reservationDate)  values (?,?,?,?)";
+  db.query(
+    sql,
+    [idClient, idDestination, numberOfPersons, date],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ err: err });
+      }
+      res.status(200).send(result);
+    }
+  );
+});
+// get all the reservations
+app.get("/reservations", (req, res) => {
+  const sql = "Select * from Reservation";
+  db.query(sql, (err, result) => {
+    if (err) {
+      return res.json({ err: err });
+    }
+    res.json(result);
+  });
+});
 app.listen(5000, () => {
   console.log("App running at port 5000");
 });
